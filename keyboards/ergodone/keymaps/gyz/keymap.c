6bca45b7df172bc6fe151e51263a3d1c5ea52814
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2025-05-31 03:02:02 +0800

    KC_SLEP

--- keyboards/ergodone/keymaps/gyz/keymap.c
+++ keyboards/ergodone/keymaps/gyz/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                       LCTL_T(KC_SPC),   LGUI_T(KC_BSPC),    LT(2,KC_ENT),
 
 //=====================right=====================
-          KC_APP,            KC_6,            KC_7,            KC_8,            KC_9,            KC_0,         KC_BSPC,
+          KC_APP,            KC_6,            KC_7,            KC_8,            KC_9,            KC_0,         KC_SLEP,
          KC_RBRC,            KC_Y,            KC_U,            KC_I,            KC_O,            KC_P,         KC_BSLS,
                              KC_H,            KC_J,            KC_K,            KC_L,         KC_SCLN,         KC_QUOT,
    TD(R_PRN_ABK),            KC_N,            KC_M,         KC_COMM,          KC_DOT,         KC_SLSH,         KC_RSFT,
