8d93927a0b7eb9cf701ff478b26dba0d5fcd42e8
Guo Yongzhi <guo-yong-zhi@outlook.com>
2025-06-16 10:33:54 +0800

    LCTL_T(KC_SPC), LGUI_T(KC_BSPC)

--- keyboards/ktec/ergodone/keymaps/gyz/keymap.c
+++ keyboards/ktec/ergodone/keymaps/gyz/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_LCTL,         KC_LGUI,         KC_LALT,         KC_MINS,          KC_EQL,
                                                                                                KC_INS,         KC_PSCR,
                                                                                                                 KC_DEL,
-                                                                      RCTL_T(KC_SPC), RGUI_T(KC_BSPC),    LT(2,KC_ENT),
+                                                                      LCTL_T(KC_SPC), LGUI_T(KC_BSPC),    LT(2,KC_ENT),
 
 //=====================right=====================
           KC_APP,            KC_6,            KC_7,            KC_8,            KC_9,            KC_0,  TD(SUPER_LOCK),
